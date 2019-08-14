fed10524908ae46d45c2435c83d2a16d845d1117
PrasadBylapudi <31813770+prasadbylapudi@users.noreply.github.com>
2019-08-14 08:50:46 +0530

    dictionary

---
+++ Dictionary/dictionary_len_method.py
@@ -0,0 +1,2 @@
+Employee = {"Name": "John", "Age": 29, "salary":25000,"Company":"GOOGLE"}  
+print(len(Employee));
---
+++ Dictionary/iterating_dictionary.py
@@ -0,0 +1,9 @@
+employee={"name":"prasad","age":19,"salary":20000,"comapny":"smartbro"};
+for x in employee:
+    print(x);#this is for printing the keys
+
+
+
+#this is for printing  the values
+for x in employee:  
+    print(employee[x]);  
